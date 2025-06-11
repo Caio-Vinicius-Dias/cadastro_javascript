c90842872dbe4b79b44cde977aad6a96741a43c9
Caio-Vinicius-Dias <caiodiasprofissional@gmail.com>
2025-06-11 10:21:24 -0300

    Atualizando o script do JavaScript

--- atividade_cadastro_javascript/script.js
+++ atividade_cadastro_javascript/script.js
@@ -1,17 +1,37 @@
-
 let nome = prompt("Digite seu nome completo:");
 let email = prompt("Digite seu e-mail:");
-let senha = prompt("Digite sua senha (mínimo 6 caracteres):");
+let senha = prompt("Digite sua senha (MÍNIMO 6 CARACTERES):");
 
 nome = nome.trim();
 email = email.trim();
 senha = senha.trim();
 
-
 let nomeMaiusculo = nome.toUpperCase();
+let emailValido = email.includes("@") && email.includes(".com");
+let nomeComUnderline = nome.replaceAll(" ", "_");
+let senhaValida = senha.length >= 6;
 
+let mensagemErro = "";
 
-let emailValido = email.includes('@') && email.includes('.com');
-
-let nomeFormatado = nome.replaceAll(' ', '_');
-
+if (!emailValido || !senhaValida) {
+    
+    mensagemErro = "Cadastro inválido. Verifique os seguintes campos:\n";
+    if (!emailValido) {
+        mensagemErro += " - E-mail deve conter '@' e '.com'\n";
+    }
+    if (!senhaValida) {
+        mensagemErro += " - Senha deve ter no mínimo 6 caracteres\n";
+    }
+    alert(mensagemErro);
+} else 
+{
+    alert('Cadastro realizado com sucesso!\n\nNome: ${nomeMaiusculo} \nE-mail: ${email} \nSenha: ${senha}');
+   
+let mensagemFinal =
+    "Cadastro realizado com sucesso!\n\n" +
+    "Nome: " + nomeMaiusculo + "\n" +
+    "E-mail: " + email + "\n" +
+    "Senha: " + senha + "\n\n" +
+    "Obrigado por se cadastrar!";
+alert(mensagemFinal);
+}
